39d1fba667c1702d1b208f11f33081c92d0955f9
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2016-04-12 21:22:46 +0900

    初期化別ファイル化

--- Hikouki2/src/head/function.h
+++ Hikouki2/src/head/function.h
@@ -1,3 +1,6 @@
+//first_init
+void first_init(int argc, char *argv[]);
+
 //input.cpp
 GLOBAL int getStateKeyOfSmallAlphabet(unsigned char key);
 GLOBAL void checkPushKey(unsigned char key, int x, int y);
@@ -6,5 +9,8 @@ GLOBAL void getMouseMotionAndInit(int *dx, int *dy);
 GLOBAL void checkMouseMotion(int x, int y);
 
 //main.cpp
+void draw(void);
+void resize(int w, int h);
+void timer(int value);
 GLOBAL void ErrorOut(const char file[], const char func[], int line,
 		const char message[]);
--- Hikouki2/src/source/main.cpp
+++ Hikouki2/src/source/main.cpp
@@ -57,37 +57,9 @@ void timer(int value) {
 	glutPostRedisplay(); //再描画
 }
 
-//初期化
-void init(int argc, char *argv[]) {
-
-	//openGLの初期化,ウィンドウ生成
-	glutInitWindowSize(WINDOW_INI_HEIGHT, WINDOW_INI_WIDTH);
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutCreateWindow(argv[0]);
-
-	//openGLイベント関数
-	glutDisplayFunc(draw);
-	glutKeyboardFunc(checkPushKey);
-	glutKeyboardUpFunc(checkUpkey);
-	glutPassiveMotionFunc(checkMouseMotion);
-	glutMotionFunc(checkMouseMotion);
-	glutReshapeFunc(resize);
-	glutTimerFunc(100, timer, 0);
-
-	//その他openGLの設定
-	glutIgnoreKeyRepeat(GL_TRUE); //繰り返しのキー入力を無視する
-	glutSetCursor(GLUT_CURSOR_CROSSHAIR);
-	glClearColor(1.0, 1.0, 1.0, 0.0); //塗りつぶし色を白に設定
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-}
-
 //メイン
 int main(int argc, char *argv[]) {
-	init(argc, argv);
+	first_init(argc, argv);
 	glutMainLoop();
 	return 0;
 }
