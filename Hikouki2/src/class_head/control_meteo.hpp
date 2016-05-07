19dade1e5ab5ae953996c2d2961583ca6f0631d0
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2016-05-07 15:59:46 +0900

    上げ忘れ

---
+++ Hikouki2/src/class_head/control_meteo.hpp
@@ -0,0 +1,4 @@
+namespace control_meteo {
+void Init();
+void Update();
+}
---
+++ Hikouki2/src/source/control_meteo.cpp
@@ -0,0 +1,14 @@
+#include "../head/GV.h"
+
+namespace {
+std::vector<Meteo*> meteo_;
+}
+
+namespace control_meteo {
+void Init() {
+	meteo_.pop_back();
+}
+void Update() {
+
+}
+}
