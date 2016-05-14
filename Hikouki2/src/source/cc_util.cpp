18d319fdb404db8864efd48fd89aec5f9948011f
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2016-05-15 05:04:29 +0900

    乱数変更

--- Hikouki2/src/source/cc_util.cpp
+++ Hikouki2/src/source/cc_util.cpp
@@ -1,13 +1,15 @@
 #include "../head/GV.h"
+namespace {
+bool has_inited_srand = false;
+}
 
 namespace cc_util {
 
 //乱数を取得する
 int GetRandom(int min, int max) {
-	static bool has_inited = false;
-	if (!has_inited) {
+	if (!has_inited_srand) {
 		srand((unsigned int) time(NULL));
-		has_inited = true;
+		has_inited_srand = true;
 		rand(); //何故か最初だけ65固定になっていたのでここで一度呼び出しておく
 	}
 	return min + (int) (rand() * (max - min + 1.0) / (1.0 + RAND_MAX));
